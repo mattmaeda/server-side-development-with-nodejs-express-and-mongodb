78f6cec1ee8a4c8be312e192aea36798d91abe2b
Matt Maeda <matt@casamaeda.com>
2021-12-23 21:06:37 -0800

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '241590524772824',
+        clientSecret: '53b126a5ef5214951aa10903de40a808'
+    }
 }
